1ce9c9a580bea99a6caa1ac019b90b1390eaf7f1
raghu3111x <74596785+raghu3111x@users.noreply.github.com>
2021-05-15 19:45:38 +0530

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,11 @@
+import os 
+modules = 'pyautogui numpy pandas OS-Platform pyfiglet pyttsx3 pywhatkit selenium'
+print("Python Modules to be installed: ")
+list = modules.split()
+print(list)
+for i in range(0,len(list)):
+	print(str(i+1) + '. ' + list[i])
+
+print()
+os.system('pip install ' + modules)
+print('\n'+'DONE installing pkgs...')
