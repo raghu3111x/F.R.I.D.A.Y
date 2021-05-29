5616010ca1370c03c8337fc8ccd938afd04ab74a
raghu3111x <74596785+raghu3111x@users.noreply.github.com>
2021-05-29 10:36:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,8 +38,8 @@ def say(t):
 	engine.runAndWait()
 def send_mail(send_to,text):
 	import smtplib
-	username="tony60687@gmail.com"
-	passwd = "!2021rohan2021!"
+	username = input('Enter the username, sir' + '\n' + ': ')
+	passwd = input('Enter the password: ')
 	smtpObj = smtplib.SMTP('smtp.gmail.com',587)
 	smtpObj.ehlo()
 	smtpObj.starttls()
