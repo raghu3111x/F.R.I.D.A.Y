c984512b0a7ea0c653aa61b0511f774a8f374782
raghu3111x <74596785+raghu3111x@users.noreply.github.com>
2021-06-04 09:58:03 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,5 +1,5 @@
 import os 
-modules = 'pyautogui numpy pandas OS-Platform pyfiglet pyttsx3 pywhatkit selenium'
+modules = 'pyautogui numpy pandas OS-Platform pyfiglet pyttsx3 pywhatkit selenium pyjokes playsound secure-smtplib wikipedia matplotlib pyPdf PyPDF2 docx'
 print("Python Modules to be installed: ")
 list = modules.split()
 print(list)
@@ -7,6 +7,6 @@ for i in range(0,len(list)):
 	print(str(i+1) + '. ' + list[i])
 
 print()
+os.system('color 0a')
 os.system('pip install ' + modules)
-os.system('pip install automateboringstuff')
 print('\n'+'DONE installing pkgs...')
